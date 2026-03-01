b624ee2074c0ebaa610cc85a9cf101ed56373d0b
FilipeVBF <filipevbf@gmail.com>
2026-03-01 09:21:42 -0300

    refactor(auth): use factories in authenticate and register controllers

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,11 +1,10 @@
 import type { FastifyReply, FastifyRequest } from "fastify";
 import z from "zod";
 
-import { RegisterUseCase } from "@/use-cases/register.js";
-import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository.js";
 import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error.js";
+import { makeRegisterUseCase } from "@/use-cases/factories/make-register-use-case.js";
 
-export async function register (request: FastifyRequest, reply: FastifyReply) {
+export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
     name: z.string(),
     email: z.email(),
@@ -14,22 +13,20 @@ export async function register (request: FastifyRequest, reply: FastifyReply) {
   const { name, email, password } = registerBodySchema.parse(request.body);
 
   try {
-    const usersRepository = new PrismaUsersRepository();
-    const registerUseCase = new RegisterUseCase(usersRepository);
+    const registerUseCase = makeRegisterUseCase();
 
-    await registerUseCase.execute({ 
+    await registerUseCase.execute({
       name,
       email,
-      password
-    })
+      password,
+    });
   } catch (err) {
     if (err instanceof UserAlreadyExistsError) {
-      return reply.status(409).send({ message: err.message })
+      return reply.status(409).send({ message: err.message });
     }
 
     throw err;
   }
-
 
   return reply.status(201).send();
 }
