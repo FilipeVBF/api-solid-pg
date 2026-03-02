c3474cf98279065f754803aa6488734994e9bcc6
FilipeVBF <filipevbf@gmail.com>
2026-03-02 18:58:53 -0300

    feat(check-in): implement fetch user check-ins history with pagination
    - add findManyByUserId to CheckInsRepository
    - implement pagination in InMemoryCheckInsRepository
    - add FetchUserCheckInsHistoryUseCase
    - add unit tests for history and pagination

---
+++ src/use-cases/fetch-user-check-ins-history.spec.ts
@@ -0,0 +1,59 @@
+import { expect, describe, it, beforeEach, vi, afterEach } from "vitest";
+import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository.js";
+import { InMemoryGymsRepository } from "@/repositories/in-memory/in-memory-gyms-repository.js";
+import { FetchUserCheckInsHistoryUseCase } from "./fetch-user-check-ins-history.js";
+
+let checkInsRepository: InMemoryCheckInsRepository;
+let sut: FetchUserCheckInsHistoryUseCase;
+
+describe("Fetch User Check-ins History Use Case", () => {
+  beforeEach(async () => {
+    checkInsRepository = new InMemoryCheckInsRepository();
+    sut = new FetchUserCheckInsHistoryUseCase(checkInsRepository);
+  });
+
+  it("should be able to fetch check-ins history", async () => {
+    await checkInsRepository.create({
+      gym_id: "gym-1",
+      user_id: "user-1",
+    });
+
+    await checkInsRepository.create({
+      gym_id: "gym-2",
+      user_id: "user-1",
+    });
+
+    const { checkIns } = await sut.execute({
+      userId: "user-1",
+      page: 1,
+    });
+
+    expect(checkIns).toHaveLength(2);
+    expect(checkIns).toEqual([
+      expect.objectContaining({ gym_id: "gym-1", user_id: "user-1" }),
+      expect.objectContaining({ gym_id: "gym-2", user_id: "user-1" }),
+    ]);
+  });
+
+  it("should be able to fetch paginated check-ins history", async () => {
+    for (let i = 1; i <= 22; i++) {
+      await checkInsRepository.create({
+        gym_id: `gym-${i}`,
+        user_id: "user-1",
+      });
+    }
+
+    const { checkIns } = await sut.execute({
+      userId: "user-1",
+      page: 2,
+    });
+
+    console.log(checkIns);
+
+    expect(checkIns).toHaveLength(2);
+    expect(checkIns).toEqual([
+      expect.objectContaining({ gym_id: "gym-21", user_id: "user-1" }),
+      expect.objectContaining({ gym_id: "gym-22", user_id: "user-1" }),
+    ]);
+  });
+});
