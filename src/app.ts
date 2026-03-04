1b978dd48469b9d2a9709614774ece65a99f10a7
FilipeVBF <filipevbf@gmail.com>
2026-03-04 17:42:07 -0300

    feat(auth): add JWT authentication and protected profile route
    - register @fastify/jwt plugin
    - add JWT_SECRET to env schema
    - extend FastifyJWT types
    - generate JWT on authenticate
    - add protected /me route
    - create profile controller (initial version)

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,15 @@ import fastify from "fastify";
 import { appRoutes } from "./http/routes.js";
 import z, { ZodError } from "zod";
 import { env } from "./env/index.js";
+import fastifyJwt from "@fastify/jwt";
 
 export const app = fastify();
 
-app.register(appRoutes)
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+});
+
+app.register(appRoutes);
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
@@ -19,6 +24,6 @@ app.setErrorHandler((error, _, reply) => {
   } else {
     // TODO: Here we should log to an external tool like DataDog/NewRelic/Sentry
   }
-  
+
   return reply.status(500).send({ message: "Internal server error" });
-})
+});
--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,9 +1,7 @@
 import type { FastifyReply, FastifyRequest } from "fastify";
 import z from "zod";
 
-import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository.js";
 import { InvalidCredentialsError } from "@/use-cases/errors/invalid-credentials-error.js";
-import { AuthenticateUseCase } from "@/use-cases/authenticate.js";
 import { makeAuthenticateUseCase } from "@/use-cases/factories/make-authenticate-use-case.js";
 
 export async function authenticate(
@@ -20,10 +18,21 @@ export async function authenticate(
   try {
     const authenticateUseCase = makeAuthenticateUseCase();
 
-    await authenticateUseCase.execute({
+    const { user } = await authenticateUseCase.execute({
       email,
       password,
     });
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      },
+    );
+
+    return reply.status(200).send({ token });
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: err.message });
@@ -31,6 +40,4 @@ export async function authenticate(
 
     throw err;
   }
-
-  return reply.status(200).send();
 }
