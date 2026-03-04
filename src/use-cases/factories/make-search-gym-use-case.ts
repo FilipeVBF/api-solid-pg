918e4645fe6342eab49163c2225da98ec3750246
FilipeVBF <filipevbf@gmail.com>
2026-03-04 15:14:33 -0300

    feat(factories): add factories for remaining use cases
    - add makeCheckInUseCase
    - add makeCreateGymUseCase
    - add makeFetchNearbyGymsUseCase
    - add makeFetchUserCheckInsHistoryUseCase
    - add makeGetUserMetricsUseCase
    - add makeGetUserProfileUseCase
    - add makeSearchGymUseCase
    - add makeValidateCheckInUseCase

---
+++ src/use-cases/factories/make-search-gym-use-case.ts
@@ -0,0 +1,9 @@
+import { PrismaGymsRepository } from "@/repositories/prisma/prisma-gyms-repository.js";
+import { SearchGymUseCase } from "../search-gym.js";
+
+export function makeSearchGymUseCase() {
+  const gymsRepository = new PrismaGymsRepository();
+  const useCase = new SearchGymUseCase(gymsRepository);
+
+  return useCase;
+}
