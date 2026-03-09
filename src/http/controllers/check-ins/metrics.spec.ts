091f33ebf1f3923e783d21f732a721d6cde3c954
FilipeVBF <filipevbf@gmail.com>
2026-03-09 10:12:41 -0300

    test(check-ins): add HTTP tests for history, metrics and validate, and update create test
    - Added tests:
      - http/controllers/check-ins/history.spec.ts
      - http/controllers/check-ins/metrics.spec.ts
      - http/controllers/check-ins/validate.spec.ts
    - Updated http/controllers/check-ins/create.spec.ts

--- src/http/controllers/check-ins/metrics.spec.ts
+++ src/http/controllers/check-ins/metrics.spec.ts
@@ -4,7 +4,7 @@ import { createAndAuthenticateUse } from "@/utils/test/create-and-authenticate-u
 import request from "supertest";
 import { afterAll, beforeAll, describe, expect, it } from "vitest";
 
-describe("Create Check-in (e2e)", () => {
+describe("Check-in Metrics (e2e)", () => {
   beforeAll(async () => {
     await app.ready();
   });
@@ -13,8 +13,10 @@ describe("Create Check-in (e2e)", () => {
     await app.close();
   });
 
-  it("should be able to create a check-in", async () => {
+  it("should be able to get the total count of check-ins", async () => {
     const { token } = await createAndAuthenticateUse(app);
+
+    const user = await prisma.user.findFirstOrThrow();
 
     const gym = await prisma.gym.create({
       data: {
@@ -24,14 +26,25 @@ describe("Create Check-in (e2e)", () => {
       },
     });
 
-    const response = await request(app.server)
-      .post(`/gyms/${gym.id}/check-ins`)
-      .set("Authorization", `Bearer ${token}`)
-      .send({
-        latitude: -13.0120686,
-        longitude: -38.4686105,
-      });
+    await prisma.checkIn.createMany({
+      data: [
+        {
+          gym_id: gym.id,
+          user_id: user.id,
+        },
+        {
+          gym_id: gym.id,
+          user_id: user.id,
+        },
+      ],
+    });
 
-    expect(response.statusCode).toEqual(201);
+    const response = await request(app.server)
+      .get("/check-ins/metrics")
+      .set("Authorization", `Bearer ${token}`)
+      .send();
+
+    expect(response.statusCode).toEqual(200);
+    expect(response.body.checkInsCount).toEqual(2);
   });
 });
--- src/http/controllers/check-ins/validate.spec.ts
+++ src/http/controllers/check-ins/validate.spec.ts
@@ -4,7 +4,7 @@ import { createAndAuthenticateUse } from "@/utils/test/create-and-authenticate-u
 import request from "supertest";
 import { afterAll, beforeAll, describe, expect, it } from "vitest";
 
-describe("Create Check-in (e2e)", () => {
+describe("Validate Check-in (e2e)", () => {
   beforeAll(async () => {
     await app.ready();
   });
@@ -13,8 +13,10 @@ describe("Create Check-in (e2e)", () => {
     await app.close();
   });
 
-  it("should be able to create a check-in", async () => {
+  it("should be able to validate a check-in", async () => {
     const { token } = await createAndAuthenticateUse(app);
+
+    const user = await prisma.user.findFirstOrThrow();
 
     const gym = await prisma.gym.create({
       data: {
@@ -24,14 +26,26 @@ describe("Create Check-in (e2e)", () => {
       },
     });
 
-    const response = await request(app.server)
-      .post(`/gyms/${gym.id}/check-ins`)
-      .set("Authorization", `Bearer ${token}`)
-      .send({
-        latitude: -13.0120686,
-        longitude: -38.4686105,
-      });
+    let checkIn = await prisma.checkIn.create({
+      data: {
+        gym_id: gym.id,
+        user_id: user.id,
+      },
+    });
 
-    expect(response.statusCode).toEqual(201);
+    const response = await request(app.server)
+      .patch(`/check-ins/${checkIn.id}/validate`)
+      .set("Authorization", `Bearer ${token}`)
+      .send();
+
+    expect(response.statusCode).toEqual(204);
+
+    checkIn = await prisma.checkIn.findUniqueOrThrow({
+      where: {
+        id: checkIn.id,
+      },
+    });
+
+    expect(checkIn.validated_at).toEqual(expect.any(Date));
   });
 });
